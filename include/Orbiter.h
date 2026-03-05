f4fee3fc66d9583cead4d2e8791a3cd3d0ab7a6c
jaronie <[200~Jisaa@BALONEY MINGW64 ~$ git config --global user.name jaronieJisaa@BALONEY MINGW64 ~$ git config --global user.email>
2026-03-05 05:35:12 +0000

    Orbiter.h header file made.

---
+++ include/Orbiter.h
@@ -0,0 +1,43 @@
+#ifndef ORBITER_H
+#define ORBITER_H
+
+
+#include <bn_fixed_point.h>
+#include <bn_sprite_ptr.h>
+#include <bn_sprite_items_dot.h>
+
+
+#include "Center.h"
+#include "movement.h"
+
+/**
+ * An object that orbits around a center. Orbits using Hooke's law, as if attached by a 2D spring.
+ * The stiffness of the spring in each dimension is center.mass() / FORCE_SCALE. 
+ */
+class Orbiter {
+    public:
+        /**
+         * Creates an orbiter.
+         * 
+         * @param starting_location the initial location of the Orbiter
+         * @param starting_velocity the initial velocity of the Orbiter, {dx, dy}
+         * @param center the center to orbit around
+         */
+        Orbiter(bn::fixed_point starting_location, bn::fixed_point starting_velocity, Center &center) :
+        _sprite(bn::sprite_items::dot.create_sprite(starting_location)),
+        _velocity(starting_velocity),
+        _center(center) {
+        }
+
+        /**
+         * Take a step orbiting around the center.
+         */
+        void update();
+
+    private:
+        bn::sprite_ptr _sprite;
+        bn::fixed_point _velocity;
+        Center& _center;
+};
+
+#endif
