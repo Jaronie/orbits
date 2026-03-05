1e6435e5d3d2a939048d1299df65ba2fb8295772
jaronie <[200~Jisaa@BALONEY MINGW64 ~$ git config --global user.name jaronieJisaa@BALONEY MINGW64 ~$ git config --global user.email>
2026-03-05 05:51:44 +0000

    Instance variables moved

--- include/Orbiter.h
+++ include/Orbiter.h
@@ -23,11 +23,7 @@ class Orbiter {
          * @param starting_velocity the initial velocity of the Orbiter, {dx, dy}
          * @param center the center to orbit around
          */
-        Orbiter(bn::fixed_point starting_location, bn::fixed_point starting_velocity, Center &center) :
-        _sprite(bn::sprite_items::dot.create_sprite(starting_location)),
-        _velocity(starting_velocity),
-        _center(center) {
-        }
+        Orbiter(bn::fixed_point starting_location, bn::fixed_point starting_velocity, Center &center);
 
         /**
          * Take a step orbiting around the center.
